69553b4cbf03a1d3fd3d9839cc68052925dce4cd
KevinNguyen10 <77524763+KevinNguyen10@users.noreply.github.com>
2022-04-16 01:36:25 -0500

    Add files via upload

---
+++ nycCrimeDataCollection.py
@@ -0,0 +1,56 @@
+# %% 
+# import necesary libraries
+import requests
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.express as px
+
+# %%
+# create a function to get the data from the API
+crime_data_NYC = 'https://data.cityofnewyork.us/resource/5uac-w243.json'
+
+def get_data(url):
+    response = requests.get(url) # Telling requests to get the data from the url, it will package the information in the response variable
+    # error handling
+    try:
+        response.raise_for_status()
+        # raise an exception if the request fails
+    except requests.exceptions.HTTPError as err:
+        return "Error: " + str(err)
+    
+    # if the status code is 200, then the request was successful
+    raw_data = response.json() # the data is in json format
+
+    # returns a list of dictionaries, so lets return just the dictionaries
+    parsed_data = {}
+    for index, item in enumerate(raw_data):
+        # key is the index of the dictionary from the list, value is the dictionary itself
+        parsed_data[index] = item
+
+    return parsed_data
+
+data = get_data(crime_data_NYC)
+
+# %%
+data
+# %%
+keys_to_keep = ['cmplnt_num', 'cmplnt_fr_dt', 'crm_atpt_cptd_cd', 
+                'juris_desc', 'law_cat_cd', 'loc_of_occur_desc', 
+                'ofns_desc', 'susp_age_group', 'susp_race', 'susp_sex', 
+                'vic_age_group', 'vic_race', 'vic_sex', 'latitude', 'longitude']
+# delete keys if its not in keys_to_keep
+new_list = []
+for k, v in data.items():
+    for k1, v2 in v.items():
+        if (k1 in keys_to_keep):
+            new_list.append(k1)
+            new_list.append(v2)
+
+# for every 2 items in the list create a list of 2 items
+list_1 = []
+for i in range(0, len(new_list), 2):
+    list_1.append(new_list[i:i+2])
+list_1
+
+# %%
